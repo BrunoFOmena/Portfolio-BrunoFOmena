35389c85c892d759fb26e3664d8119574c63df38
Bruno <brunofelicianodeomena@gmail.com>
2026-01-02 11:19:53 -0300

    change

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio-BrunoFOmena">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/thoughts/:id" element={<ThoughtDetail />} />
